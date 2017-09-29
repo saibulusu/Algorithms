6aca536c1d1ec0f10edc9dae8ad7f93c78faa455
Sai Bulusu <sai.bulusu@gmail.com>
2017-09-28 17:11:29 -0700

    Valid Parentheses

--- src/Strings/ValidParentheses.java
+++ src/Strings/ValidParentheses.java
@@ -4,18 +4,26 @@ import org.junit.Test;
 
 public class ValidParentheses {
 
+	//check if an orientation of parentheses is valid
 	public static boolean isValid(String string){
 		int count = 0;
+		//start count at 0, which resets to 0 every time the string is valid
 		for(int i = 0; i < string.length(); i++){
+			//iterate through the whole string
 			if(string.charAt(i) == '('){
+				//if it is an opening parentheses sign, increment count
 				count++;
 			} else{
+				//otherwise, if count is zero, then it is valid until this character which
+				//is a closing parentheses
 				if(count == 0){
 					return false;
 				} else {
+					//last case: decrement count, so it is still valid
 					count--;
 				}
 			}
+			//return true if the count is 0
 		} return count == 0;
 	}
 
