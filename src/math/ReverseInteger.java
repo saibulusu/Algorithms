3cc8fd2aac1a9d8db5e3f234597d11e8d23b281f
Sai Bulusu <sai.bulusu@gmail.com>
2017-10-04 18:27:54 -0700

    Reverse Integer

--- src/math/ReverseInteger.java
+++ src/math/ReverseInteger.java
@@ -3,17 +3,36 @@ package math;
 import org.junit.Test;
 
 public class ReverseInteger {
-
-	public static int reverse(int i){
-		int j = 0;
-		while(i != 0){
-			j *= 10;
-			j += i % 10;
-			i /= 10;
+	
+	//reverse the digits of a given integer x
+    public int reverse(int x) {
+    	//convert the int to a string, and set up an empty string which we will return
+		String x1 = "" + x;
+		String y1 = "";
+		//if the zeroth char is negative, disregard it for now
+		if(x1.charAt(0) == '-'){
+			x1 = x1.substring(1);
+			//iterate through and add in the opposite order to the other string
+			for(int i = 0; i < x1.length(); i++){
+				y1 += x1.charAt(x1.length() - 1 - i);
+			}
+			//add a negative if necessary
+			y1 = "-" + y1;
+		} else{
+			//if no negative is needed, just do the conversion
+			for(int i = 0; i < x1.length(); i++){
+				y1 += x1.charAt(x1.length() - 1 - i);
+			}
 		}
-		return j;
-		
-	}
+		//if converting the string back to an int returns an exception, then return 0
+		//in this case, the integer is clearly out of bounds
+		try {
+			return Integer.parseInt(y1);
+		} catch (NumberFormatException n){
+			return 0;
+		}
+
+    }
 	
 	@Test
 	public void test(){
