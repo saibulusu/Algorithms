4684bf39fe04130baa293c528837bfaf82577c64
Sai Bulusu <sai.bulusu@gmail.com>
2017-10-19 17:52:11 -0700

    Sum

---
+++ src/array/Sum.java
@@ -0,0 +1,36 @@
+package array;
+
+public class Sum {
+
+	//return the sum of every element in the array
+	public static int sum(int[] array){
+		//use the recursive method with indices 0 and array.length - 1
+		return sum(array, 0, array.length - 1);
+	}
+	
+	//recursive method to find the sum between index start and index end inclusive
+	public static int sum(int[] array, int start, int end){
+		//base case: start and end are equal -> return the current value
+		if(start == end){
+			return array[start];
+		}
+		//recursion: return the current value + this method called on everything after this value
+		return array[start] + sum(array, start + 1, end);
+	}
+	
+	
+	public void test(){
+		int[] a = {};
+		assert(sum(a) == 0);
+		
+		int[] b = {0, 1, 2, 3, 4, 5};
+		assert(sum(b) == 15);
+		
+		int[] c = {-1, 0, 1};
+		assert(sum(c) == 0);
+		
+		int[] d = {8, 7, 5, 1, 4, 2, 5, 2};
+		assert(sum(d) == 34);
+	}
+	
+}
